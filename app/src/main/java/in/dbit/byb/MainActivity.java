cc5d6acd57a4f3d40440067681a69727530c81b4
Anto <anto.christo.20@gmail.com>
2018-04-01 17:44:33 +0530

    Add test home screen

--- app/src/main/java/in/dbit/byb/MainActivity.java
+++ app/src/main/java/in/dbit/byb/MainActivity.java
@@ -2,12 +2,52 @@ package in.dbit.byb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button profile_btn;
+    Button test_btn;
+    Button resume_btn;
+    Button access_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        profile_btn = (Button) findViewById(R.id.profile_btn);
+        test_btn = (Button) findViewById(R.id.test_btn);
+        resume_btn = (Button) findViewById(R.id.resume_btn);
+        access_btn = (Button) findViewById(R.id.access_btn);
+
+        profile_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        test_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        resume_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        access_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
