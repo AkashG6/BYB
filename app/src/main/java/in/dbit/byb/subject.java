e8a483a336e45714519070aedd9df0b3adc9f91e
akash <akashgarule@gmail.com>
2018-05-20 16:40:22 +0530

    add backend connectivity

--- app/src/main/java/in/dbit/byb/subject.java
+++ app/src/main/java/in/dbit/byb/subject.java
@@ -2,12 +2,68 @@ package in.dbit.byb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.IOException;
 
 public class subject extends AppCompatActivity {
+    TextView text;
+    Button clk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject);
+
+        text = (TextView) findViewById(R.id.textView1);
+        clk = (Button) findViewById(R.id.button1);
+
+    }
+
+    public void clickhere(View v) // view is data type , v is object name
+    {
+
+        final TextView mTextView = (TextView) findViewById(R.id.text);
+        // ...
+
+        // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(this);
+                String url ="http://bybtest.000webhostapp.com/connection.php";
+
+        // Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                text.setText("Response is: "+ response);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        text.setText("That didn't work!");
+                    }
+                });
+
+        // Add the request to the RequestQueue.
+                queue.add(stringRequest);
+
     }
 }
