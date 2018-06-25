d569d7ccb7de123eca368016283d0efe6284aace
Ivan Maria <ivanmaria05@gmail.com>
2018-06-25 16:01:02 +0530

    resume - initial

--- app/src/main/java/in/dbit/byb/Resume_Main.java
+++ app/src/main/java/in/dbit/byb/Resume_Main.java
@@ -1,13 +1,29 @@
 package in.dbit.byb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+
+import in.dbit.byb.PersonalityProfiling.ProfileHome;
 
 public class Resume_Main extends AppCompatActivity {
-
+CardView btn1,btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resume__main);
+        btn1=findViewById(R.id.createresumebtn);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Resume_Main.this,Resume_Personal.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
