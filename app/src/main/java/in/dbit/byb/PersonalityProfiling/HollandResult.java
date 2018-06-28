77ef2eb7c00b20ecda3de174418a960a2c0bb358
Anto <anto.christo.20@gmail.com>
2018-06-28 17:28:30 +0530

    Add bar chart for Big5 and Holland test result.

---
+++ app/src/main/java/in/dbit/byb/PersonalityProfiling/HollandResult.java
@@ -0,0 +1,62 @@
+package in.dbit.byb.PersonalityProfiling;
+
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+
+import java.util.ArrayList;
+
+import in.dbit.byb.R;
+
+public class HollandResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_holland_result);
+
+        Bundle extras = getIntent().getExtras();
+        int Re = extras.getInt("Re");
+        int I = extras.getInt("I");
+        int A = extras.getInt("A");
+        int S = extras.getInt("S");
+        int E = extras.getInt("E");
+        int C = extras.getInt("C");
+
+        BarChart chart = (BarChart) findViewById(R.id.chart);
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0f, Re));
+        entries.add(new BarEntry(1f, I));
+        entries.add(new BarEntry(2f, A));
+        entries.add(new BarEntry(3f, S));
+        entries.add(new BarEntry(4f, E));
+        entries.add(new BarEntry(5f, C));
+
+        BarDataSet dataset = new BarDataSet(entries, "Holland Test Result");
+
+        String[] values = new String[]{"R","I","A","S","E","C"};
+
+        BarData data = new BarData(dataset);
+        chart.setData(data);
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setEnabled(true);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setGranularity(1f);
+        chart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(values));
+        xAxis.setDrawGridLines(false);
+        chart.getDescription().setEnabled(false);
+
+        chart.animateY(1500);
+
+        int color = ContextCompat.getColor(HollandResult.this, R.color.colorPrimary);
+        dataset.setColor(color);
+    }
+}
