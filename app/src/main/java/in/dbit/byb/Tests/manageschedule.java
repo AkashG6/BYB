d43dc76a0739719151e94e028547444b1e7b0a1a
AkashG6 <akashgarule@gmail.com>
2018-06-29 00:48:03 +0530

    manageSchedule page shows nearest deadline first

--- app/src/main/java/in/dbit/byb/Tests/manageschedule.java
+++ app/src/main/java/in/dbit/byb/Tests/manageschedule.java
@@ -17,6 +17,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -25,88 +26,93 @@ import in.dbit.byb.R;
 public class manageschedule extends AppCompatActivity {
 
     private void exec(String res){
-        Toast.makeText(manageschedule.this, res, Toast.LENGTH_LONG).show();
-        JSONObject obj;
+//        Toast.makeText(manageschedule.this, String.valueOf(res.length()), Toast.LENGTH_LONG).show();
         try {
-            obj = new JSONObject(res);
-            Toast.makeText(manageschedule.this, String.valueOf(obj.length()), Toast.LENGTH_LONG).show();
-            Toast.makeText(manageschedule.this, "In TRY", Toast.LENGTH_LONG).show();
 
+            JSONArray jsonArray = new JSONArray(res);
+//
+            final LinearLayout lm = (LinearLayout) findViewById(R.id.outer);
+
+            // create the layout params that will be used to define how your
+            // button will be displayed
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(0,70,0,0);
+
+            CardView.LayoutParams cmargins = new CardView.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            cmargins.setMargins(35, 35, 35, 35);
+
+            for(int j=0; j<jsonArray.length(); j++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(j);
+
+                String stask = jsonObject.getString("task");
+                Integer dday = jsonObject.getInt("deadline_day");
+                Integer dmonth = jsonObject.getInt("deadline_month");
+                Integer dyear = jsonObject.getInt("deadline_year");
+                Integer revives = jsonObject.getInt("revives_left");
+
+                // Create LinearLayout
+                CardView cv = new CardView(this);
+                cv.setLayoutParams(cmargins);
+
+                LinearLayout ll = new LinearLayout(this);
+                ll.setOrientation(LinearLayout.VERTICAL);
+                ll.setPadding(40,40, 40,40);
+
+
+                // Create TextView
+                TextView task1 = new TextView(this);
+                task1.setTextSize(18);
+                task1.setText("Task: "+stask);
+                ll.addView(task1);
+
+                // Create TextView
+                TextView deadline = new TextView(this);
+                deadline.setTextSize(18);
+                deadline.setText("Deadline:"+dday+"/"+dmonth+"/"+dyear);
+                ll.addView(deadline);
+
+                // Create TextView
+                TextView revives1 = new TextView(this);
+                revives1.setTextSize(18);
+                revives1.setText("Revives Left:"+revives);
+                ll.addView(revives1);
+
+                // Create Button
+                final Button btn = new Button(this);
+                // Give button an ID
+                btn.setId(j+1);
+                btn.setText("Submit for completion");
+                // set the layoutParams on the button
+                btn.setLayoutParams(params);
+
+                final int index = j;
+                // Set click listener for button
+                btn.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+
+                        Log.i("TAG", "index :" + index);
+
+                        Toast.makeText(getApplicationContext(),
+                                "Clicked Button Index :" + index,
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                //Add button to LinearLayout
+                ll.addView(btn);
+                cv.addView(ll);
+                //Add button to LinearLayout defined in XML
+                lm.addView(cv);
+
+            }
 
         } catch (JSONException e) {
+            Toast.makeText(manageschedule.this, String.valueOf("ERROR!!! "+e), Toast.LENGTH_LONG).show();
             Log.e("MYAPP", "unexpected JSON exception", e);
         }
 
-
-
-        final LinearLayout lm = (LinearLayout) findViewById(R.id.outer);
-
-        // create the layout params that will be used to define how your
-        // button will be displayed
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        params.setMargins(0,70,0,0);
-
-        CardView.LayoutParams cmargins = new CardView.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        cmargins.setMargins(35, 35, 35, 35);
-        for(int j=0;j<=4;j++)
-        {
-            // Create LinearLayout
-            CardView cv = new CardView(this);
-            cv.setLayoutParams(cmargins);
-
-            LinearLayout ll = new LinearLayout(this);
-            ll.setOrientation(LinearLayout.VERTICAL);
-            ll.setPadding(40,40, 40,40);
-
-
-            // Create TextView
-            TextView task = new TextView(this);
-            task.setTextSize(18);
-            task.setText("Task: "+j);
-            ll.addView(task);
-
-            // Create TextView
-            TextView deadline = new TextView(this);
-            deadline.setTextSize(18);
-            deadline.setText("Deadline:"+j);
-            ll.addView(deadline);
-
-            // Create TextView
-            TextView revives = new TextView(this);
-            revives.setTextSize(18);
-            revives.setText("Revives Left:"+j);
-            ll.addView(revives);
-
-            // Create Button
-            final Button btn = new Button(this);
-            // Give button an ID
-            btn.setId(j+1);
-            btn.setText("Submit for completion");
-            // set the layoutParams on the button
-            btn.setLayoutParams(params);
-
-            final int index = j;
-            // Set click listener for button
-            btn.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-
-                    Log.i("TAG", "index :" + index);
-
-                    Toast.makeText(getApplicationContext(),
-                            "Clicked Button Index :" + index,
-                            Toast.LENGTH_LONG).show();
-                }
-            });
-
-            //Add button to LinearLayout
-            ll.addView(btn);
-            cv.addView(ll);
-            //Add button to LinearLayout defined in XML
-            lm.addView(cv);
-
-        }
     }
 
     @Override
@@ -116,6 +122,10 @@ public class manageschedule extends AppCompatActivity {
 
         RequestQueue queue = Volley.newRequestQueue(this);
         String url ="http://bybtest.000webhostapp.com/getManageSchedule.php";
+
+
+
+
         StringRequest strreq = new StringRequest(Request.Method.GET,url,
                 new Response.Listener<String>() {
                     @Override
