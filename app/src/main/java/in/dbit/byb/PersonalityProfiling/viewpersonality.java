f619d4fb2df575caae758b5f94cd3bdeb3ecacd1
singhshyam473 <singhshyam473@gmail.com>
2018-06-29 20:07:45 +0530

    onlick for intro button

--- app/src/main/java/in/dbit/byb/PersonalityProfiling/viewpersonality.java
+++ app/src/main/java/in/dbit/byb/PersonalityProfiling/viewpersonality.java
@@ -1,7 +1,9 @@
-package in.dbit.byb;
+package in.dbit.byb.PersonalityProfiling;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import in.dbit.byb.R;
 
 public class viewpersonality extends AppCompatActivity {
 
