20f764382ff6c34367be2fe3b275e281a1527c99
Anto <anto.christo.20@gmail.com>
2018-09-14 22:24:40 +0530

    Add job JSON file. Retrieve list of jobs from JSON and show in app (not based on score).

---
+++ app/src/main/java/in/dbit/byb/PersonalityProfiling/HollandJobs.java
@@ -0,0 +1,74 @@
+package in.dbit.byb.PersonalityProfiling;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import in.dbit.byb.R;
+
+public class HollandJobs extends AppCompatActivity {
+
+    ListView jobs_list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_holland_jobs);
+
+        jobs_list = (ListView) findViewById(R.id.jobs_list);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(HollandJobs.this, android.R.layout.simple_list_item_1);
+        jobs_list.setAdapter(arrayAdapter);
+
+        try {
+            JSONObject obj = new JSONObject(loadJSONFromAsset());
+            JSONArray job_array = obj.getJSONArray("jobs");
+
+            String[] job_name = new String[job_array.length()];
+            String[] job_code = new String[job_array.length()];
+
+            for (int i = 0; i < job_array.length(); i++) {
+                JSONObject job_object = job_array.getJSONObject(i);
+//              Log.d("Details-->", jo_inside.getString("name"));
+                String name = job_object.getString("name");
+                String code = job_object.getString("code");
+
+                job_name[i] = name;
+                job_code[i] = code;
+            }
+
+            for(int i=0; i<job_array.length(); i++) {
+                arrayAdapter.add(job_name[i]);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String loadJSONFromAsset() {
+        String json = null;
+        try {
+            InputStream is = HollandJobs.this.getAssets().open("holland-jobs.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+}
--- app/src/main/java/in/dbit/byb/PersonalityProfiling/HollandResult.java
+++ app/src/main/java/in/dbit/byb/PersonalityProfiling/HollandResult.java
@@ -1,5 +1,6 @@
 package in.dbit.byb.PersonalityProfiling;
 
+import android.content.Intent;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -67,7 +68,8 @@ public class HollandResult extends AppCompatActivity {
         view_jobs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(HollandResult.this,HollandJobs.class);
+                startActivity(intent);
             }
         });
     }
