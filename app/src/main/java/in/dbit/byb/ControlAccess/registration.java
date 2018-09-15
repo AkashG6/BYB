191164ac1c894913fe016b4e4912deee32b9a9aa
AkashG6 <akashgarule@gmail.com>
2018-09-15 21:54:11 +0530

    fix student registration bug

--- app/src/main/java/in/dbit/byb/ControlAccess/registration.java
+++ app/src/main/java/in/dbit/byb/ControlAccess/registration.java
@@ -1,7 +1,6 @@
 package in.dbit.byb.ControlAccess;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
